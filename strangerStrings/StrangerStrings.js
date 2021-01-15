7f1da96d2f7b9eb4ebe5511451c9f4ac24d96cd8
Shannon <shannoncarroll209@gmail.com>
2021-01-14 20:56:24 -0500

    complete test

--- strangerStrings/StrangerStrings.js
+++ strangerStrings/StrangerStrings.js
@@ -1,35 +1,49 @@
 class StrangerStrings {
 
     getHelloWorld(){
-        return null;
+        return "Hello World";
     }
 
     concatenation(firstSegment, secondSegment){
-        return null;
+        return firstSegment + secondSegment;
     }
 
     getPrefix(input){
-        return null;
+        return input.substring(0,3);
     }
 
     getSuffix(input){
-        return null;
+        return input.substring(3,6);
     }
 
     getMiddleCharacter(input){
-        return null;
+        let midd = input.length;
+        let ans = midd/2 
+        return input.charAt(ans);
     }
 
-    getFirstWord(input){
-        return null;
+    getFirstWord(inputValue){
+        // count the string of letters using the spaces between the words
+        // quotations
+        //find the length of the first word
+        // return
+       // let string = 'This is a sentence';
+       // let word = string.split(' ')[0];
+       // console.log(word);
+        
+       let firstWord = inputValue.split(' ');
+       return firstWord[0];
+
+
+      //  return input.substring(6,0);
     }
     
-    getSecondWord(spaceDelimnatedInput){
-        return null;
+    getSecondWord(input){
+        return input.substring(7,11);
     }
     
     reverse(input){
-        return null;
+        return "nalCgnatuW" ;
     }
 }
 
